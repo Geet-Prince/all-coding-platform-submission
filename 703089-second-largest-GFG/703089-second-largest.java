557a0d14359292fdad8363f2f6de3f39644c22f3
Prince Raj <168627808+Geet-Prince@users.noreply.github.com>
2026-09-04 16:45:50 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 703089-second-largest-GFG/703089-second-largest.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest=Integer.MIN_VALUE;
+        int seclargest=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(largest<arr[i]){
+                seclargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>seclargest&&arr[i]!=largest){
+                seclargest=arr[i];
+            }
+            
+            
+        }
+        if(seclargest==Integer.MIN_VALUE){
+            return -1;
+        }
+        return seclargest;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
